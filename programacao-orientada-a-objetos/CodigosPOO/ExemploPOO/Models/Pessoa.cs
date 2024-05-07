c60283eafd1e809809b956b013928b88931c4a52
RodrigoRamone7 <rodrigo_wow7@hotmail.com>
2024-05-07 08:34:34 -0300

    Test: testando índice em manipulando-dados-objetos

---
+++ programacao-orientada-a-objetos/CodigosPOO/ExemploPOO/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá! Meu nome é {Nome}");
+            Console.WriteLine($"E tenho {Idade} anos");
+        }
+    }
+}
---
+++ programacao-orientada-a-objetos/CodigosPOO/ExemploPOO/Program.cs
@@ -0,0 +1,6 @@
+﻿using ExemploPOO.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Rodrigo";
+p1.Idade = 29;
+p1.Apresentar();
